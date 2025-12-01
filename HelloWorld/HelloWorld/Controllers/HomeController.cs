ee2a2b1fd2cab79fb7cf3d3c24edabf945d406d5
zcomert <zafer.comert@hotmail.com>
2025-12-01 14:51:15 +0300

    Route data parameter has been added.

--- HelloWorld/HelloWorld/Controllers/HomeController.cs
+++ HelloWorld/HelloWorld/Controllers/HomeController.cs
@@ -11,4 +11,32 @@ public class HomeController : ControllerBase
     {
         return "Hello, World from HomeController.";
     }
+
+    [HttpGet("students")] // api/home/students
+    public List<String> GetStudents()
+    {
+        var students =  new List<String>()
+        {
+            "Mehmet",
+            "Hakan",
+            "Murat",
+            "Selahattin",
+            "Murat"
+        };
+        return students;
+    }
+
+    [HttpGet("students/{id}")] // api/home/students/{id}
+    public String GetOneStudent(int id)
+    {
+        var students = new List<String>()
+        {
+            "Mehmet",
+            "Hakan",
+            "Murat",
+            "Selahattin",
+            "Murat"
+        };
+        return students[id];
+    }
 }
