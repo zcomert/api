d8b5675d7b29e845e563acabe19b7973b65eaff8
zcomert <zafer.comert@hotmail.com>
2025-12-01 15:37:54 +0300

    Swagger has been added.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -2,10 +2,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // container
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.MapControllers();
+
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/selam", () => "Merhaba Zafer!");
