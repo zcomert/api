8e80313ee22a5cc595becc24819b9eb3462604cc
zcomert <zafer.comert@hotmail.com>
2025-12-01 14:25:05 +0300

    Pipeline has been examined.

---
+++ HelloWorld/HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HomeController : ControllerBase
+{
+    [HttpGet]
+    public String Greetings()
+    {
+        return "Hello, World from HomeController.";
+    }
+}
--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// container
+builder.Services.AddControllers();
+
 var app = builder.Build();
+
+app.MapControllers();
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/selam", () => "Merhaba Zafer!");
+
 app.Run();
