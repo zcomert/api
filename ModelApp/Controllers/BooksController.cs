f2b5ce228a7ac042e6d041be3df879850f475677
zcomert <zafer.comert@hotmail.com>
2025-12-02 11:03:59 +0300

    InMemory has been started.

--- ModelApp/Controllers/BooksController.cs
+++ ModelApp/Controllers/BooksController.cs
@@ -1,6 +1,20 @@
-﻿namespace ModelApp.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelApp.Data;
+using ModelApp.Models;
+
+namespace ModelApp.Controllers
 {
-    public class BooksController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var list = ApplicationContextInMemory.Books;
+            if (list.Count.Equals(0))
+                return NoContent(); // 204
+            return Ok(list);
+        }
     }
 }
