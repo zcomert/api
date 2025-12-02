8d50855dc7f601fdc130a1d0b94e09375395c84a
zcomert <zafer.comert@hotmail.com>
2025-12-02 12:00:46 +0300

    CategoriesController has been updated.

--- ModelApp/Controllers/BooksController.cs
+++ ModelApp/Controllers/BooksController.cs
@@ -2,19 +2,63 @@
 using ModelApp.Data;
 using ModelApp.Models;
 
-namespace ModelApp.Controllers
+namespace ModelApp.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BooksController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class BooksController : ControllerBase
+    [HttpGet] // api/books
+    public IActionResult GetAllBooks()
     {
-        [HttpGet]
-        public IActionResult GetAllBooks()
+        var list = ApplicationContextInMemory.Books;
+        if (list.Count.Equals(0))
+            return NoContent(); // 204
+        return Ok(list);
+    }
+
+    [HttpGet("{id:int}")] // api/books/{id}
+    public IActionResult GetOneBook([FromRoute(Name ="id")] int id)
+    {
+        // Eski kod yapısı : Prosedürel / Yapılandırılmış programlama paradigması
+        //foreach (Book book in ApplicationContextInMemory.Books)
+        //{
+        //    if (book.Id == id)
+        //        return Ok(book); // 200
+        //}
+        //return NotFound(); // 404
+
+        var book = ApplicationContextInMemory
+            .Books
+            .FirstOrDefault(b => b.Id == id);
+        if (book is null)
+            return NotFound(); // 404
+        return Ok(book); // 200
+    }
+
+    [HttpPost]
+    public IActionResult CreateOneBook([FromBody] Book book)
+    {
+        try
         {
-            var list = ApplicationContextInMemory.Books;
-            if (list.Count.Equals(0))
-                return NoContent(); // 204
-            return Ok(list);
+            int max = 0;
+            foreach (Book b in ApplicationContextInMemory.Books)
+            {
+                if (b.Id > max)
+                    max = b.Id;
+            }
+
+            //var max = ApplicationContextInMemory
+            //    .Books
+            //    .Max(b => b.Id);
+
+            book.Id = max + 1;
+            ApplicationContextInMemory.Books.Add(book);
+            return StatusCode(201, book); // 201
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message); // 400
         }
     }
 }
--- ModelApp/Controllers/CategoriesController.cs
+++ ModelApp/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ModelApp.Data;
+using ModelApp.Models;
 
 namespace ModelApp.Controllers
 {
@@ -11,7 +12,58 @@ namespace ModelApp.Controllers
         [HttpGet]
         public IActionResult GetAllCategories()
         {
+            var categories = ApplicationContextInMemory.Categories;
+            if (categories.Count == 0)
+            {
+                return NotFound("Herhangi bir kategori bulunamadı.");
+            }
             return Ok(ApplicationContextInMemory.Categories);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneCategory([FromRoute(Name = "id")] int id)
+        {
+            var category = ApplicationContextInMemory
+                .Categories
+                .FirstOrDefault(c => c.CategoryId == id);
+
+            if (category is null)
+            {
+                return NotFound($"ID'si {id} olan kategori bulunamadı.");
+            }
+
+            return Ok(category);
+        }
+
+        [HttpPost] // api/categories
+        public IActionResult CreateOneCategory([FromBody] Category category)
+        {
+            try
+            {
+                // dot per line 
+                category.CategoryId = ApplicationContextInMemory
+                    .Categories
+                    .Max(c => c.CategoryId) + 1;
+
+                ApplicationContextInMemory.Categories.Add(category);
+                return CreatedAtAction(
+                    nameof(GetOneCategory),
+                    new { id = category.CategoryId },
+                    category);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    Message = "Kategori oluşturulurken bir hata oluştu.",
+                    Error = ex.Message,
+                    Time = DateTime.Now,
+                    URI = HttpContext.Request.Path
+                });
+                
+            }
+            
+
         }
     }
 }
