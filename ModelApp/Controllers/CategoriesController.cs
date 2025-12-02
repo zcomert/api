a67eadf62ab819bd83539e5845b2dac2e18adfcc
zcomert <zafer.comert@hotmail.com>
2025-12-02 11:09:57 +0300

    Categories controller has been added.

---
+++ ModelApp/Controllers/CategoriesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelApp.Data;
+
+namespace ModelApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllCategories()
+        {
+            return Ok(ApplicationContextInMemory.Categories);
+        }
+    }
+}
