11cbe03decfa8c8a63c45b25952e7cb3cfb47e1a
zcomert <zafer.comert@hotmail.com>
2025-12-03 11:08:23 +0300

    MSSQL has been applied with ORM (EF Core)

--- ContextDemoApp/Program.cs
+++ ContextDemoApp/Program.cs
@@ -1,18 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<RepositoryContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MssqlConnectionString")));
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => new List<Book>()
-{
-    new Book { Id = 1, Title = "The Great Gatsby", Price = 10.99M },
-    new Book { Id = 2, Title = "1984", Price = 8.99M },
-    new Book { Id = 3, Title = "To Kill a Mockingbird", Price = 12.49M }
-});
+app.MapGet("/", () => "Selam");
 
 app.Run();
 
-class Book
+public class Book
 {
     public int Id { get; set; }
     public String Title { get; set; }
     public decimal Price { get; set; }
+}
+
+public class RepositoryContext : DbContext
+{
+    public DbSet<Book> Books { get; set; }
+    public RepositoryContext(DbContextOptions<RepositoryContext> options)
+        : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Book>().HasData(
+            new Book { Id = 1, Title = "The Great Gatsby", Price = 10.99M },
+            new Book { Id = 2, Title = "1984", Price = 8.99M },
+            new Book { Id = 3, Title = "To Kill a Mockingbird", Price = 12.49M }
+        );  
+    }
 }
