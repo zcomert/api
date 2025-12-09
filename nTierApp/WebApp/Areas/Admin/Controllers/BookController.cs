ce9ac87b1091820b50a937568f525c2a1073087e
zcomert <zafer.comert@hotmail.com>
2025-12-09 13:37:57 +0300

    Updated.

--- nTierApp/WebApp/Areas/Admin/Controllers/BookController.cs
+++ nTierApp/WebApp/Areas/Admin/Controllers/BookController.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NLog.Config;
+using Services.Contracts;
 
 namespace WebApp.Areas.Admin.Controllers;
 
 [Area("Admin")]
 public class BookController : Controller
 {
+    private readonly IServiceManager _manager;
+
+    public BookController(IServiceManager manager)
+    {
+        _manager = manager;
+    }
+
     public IActionResult Index()
     {
-        return View();
+        var books = _manager.BookService.GetAllBooks();
+        return View(books);
     }
 }
