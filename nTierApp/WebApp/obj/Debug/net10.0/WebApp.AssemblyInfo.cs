b45784bf6b949e2f942bbdab18c1ade47f3e784b
zcomert <zafer.comert@hotmail.com>
2025-12-09 18:41:16 +0300

    End of day has been updated.

--- nTierApp/WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
+++ nTierApp/WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bc4c683c8352efa7a04679e9677ca7a78a01473")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ce1ad8c2a2b71508097d690ce8b2bde974603aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
